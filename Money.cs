ccae3fa1c4a750a21e279fb70c93ed950da325e7
Vladimir Syrykh <syrykh@corepartners.ru>
2021-03-20 14:43:29 +0300

    First implementation

---
+++ Money.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace Money
+{
+	
+	abstract class Money
+	{
+		public double Value;
+		public string Currency;
+		public Dictionary<string, double> Course;
+		public static bool operator <(Money m1, Money m2)
+		{
+			if (m1.GetType() == m2.GetType())
+				return m1.Value < m2.Value;
+			return m1.Value * m1.Course[m2.Currency] < m2.Value;
+		}
+		public static bool operator >(Money m1, Money m2)
+		{
+			if (m1.GetType() == m2.GetType())
+				return m1.Value > m2.Value;
+			return m1.Value * m1.Course[m2.Currency] > m2.Value;
+		}
+		public static UnconvertableMoney operator +(Money m1, Money m2)
+		{
+			if (m1.GetType() == m2.GetType())
+				return new UnconvertableMoney(m1.Value + m2.Value, m2.Currency);
+			return new UnconvertableMoney(m1.Value * m1.Course[m2.Currency] + m2.Value, m2.Currency);
+		}
+		public static UnconvertableMoney operator -(Money m1, Money m2)
+		{
+			if (m1.GetType() == m2.GetType())
+				return new UnconvertableMoney(m1.Value - m2.Value, m2.Currency);
+			return new UnconvertableMoney(m1.Value * m1.Course[m2.Currency] - m2.Value, m2.Currency);
+		}
+
+		public override string ToString()
+		{
+			return $"{Value}{Currency}";
+		}
+	}
+
+
+	class Rub : Money
+	{
+		public Rub(double val)
+		{
+			Currency = "R";
+			Value = val;
+			Course = new Dictionary<string, double>();
+			Course["E"] = 0.011;
+			Course["D"] = 0.013;
+		}
+	}
+
+	class Dollar : Money
+	{
+		public Dollar(double val)
+		{
+			Currency = "D";
+			Value = val;
+			Course = new Dictionary<string, double>();
+			Course["E"] = 0.84;
+			Course["R"] = 74.25;
+		}
+	}
+
+	class Euro: Money
+	{
+		public Euro(double val)
+		{
+			Currency = "E";
+			Value = val;
+			Course = new Dictionary<string, double>();
+			Course["D"] = 1.19;
+			Course["R"] = 88.72;
+
+		}
+	}
+
+	sealed class UnconvertableMoney : Money
+	{
+		new private Dictionary<string, double> Course;
+		public UnconvertableMoney(double val, string curr)
+		{
+			Value = val;
+			Currency = curr;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Money
+{
+	class Program
+	{
+		public static Money TakeMoney(string input)
+		{
+			if (input.EndsWith('R'))
+			{
+				return new Rub(int.Parse(input.Remove(input.Length - 1)));
+			}
+			if (input.EndsWith('E'))
+			{
+				return new Euro(int.Parse(input.Remove(input.Length - 1)));
+			}
+			if (input.EndsWith('D'))
+			{
+				return new Dollar(int.Parse(input.Remove(input.Length - 1)));
+			}
+			return new UnconvertableMoney(0, null);
+		}
+		static void Main(string[] args)
+		{
+			var exit = "";
+			while (exit != "stop")
+			{
+				var input = Console.ReadLine();
+				if (input == "stop") break;
+				var moneys = input.Split(new char[] {'+', '-', '>', '<', ' '}, StringSplitOptions.RemoveEmptyEntries);
+				var m1 = TakeMoney(moneys[0]);
+				var m2 = TakeMoney(moneys[1]);
+				if (input.Contains('+'))
+					Console.WriteLine(m1+m2);
+				if (input.Contains('-'))
+					Console.WriteLine(m1-m2);
+				if (input.Contains('<'))
+					Console.WriteLine(m1<m2);
+				if (input.Contains('>'))
+					Console.WriteLine(m1>m2);
+			}
+		}
+	}
+}
